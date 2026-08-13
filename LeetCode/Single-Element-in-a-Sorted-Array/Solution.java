1df1930679e061136f8cd0f373efbb04f437e6f7
Shakti Vardhan Singh <shaktivardhans9@gmail.com>
2026-08-13 09:47:57 +0530

    Add solution for Single Element in a Sorted Array

--- LeetCode/Single-Element-in-a-Sorted-Array/Solution.java
+++ LeetCode/Single-Element-in-a-Sorted-Array/Solution.java
@@ -1,14 +1,21 @@
 1class Solution {
 2    public int singleNonDuplicate(int[] nums) {
 3        int n = nums.length;
-4        if (n == 1) return nums[0];
-5            int i = 0; int h = n-1;
-6        while (i < h){
-7            if (nums[i] != nums[i + 1]) {
-8            return nums[i];
-9        }
-10            i += 2;
-11        }
-12        return nums [h];
-13    }
-14}
+4        if (n == 1){
+5            return nums[0];
+6        }
+7        int low = 0; int high = n-1;
+8        while (low<high){
+9            int mid=low+(high-low)/2;
+10            if (mid%2==1){
+11                mid--;
+12            }
+13            if(nums[mid]==nums[mid+1]){
+14                low=mid+2;
+15            } else{
+16            high=mid;
+17        }
+18    }
+19        return nums[high];
+20    }
+21}
