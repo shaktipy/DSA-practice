4b632a69c84449daa39f759620b04d3d8a125450
Shakti Vardhan Singh <shaktivardhans9@gmail.com>
2026-08-20 08:46:09 +0530

    Add solution for Rotate Array

--- LeetCode/Rotate-Array/Solution.java
+++ LeetCode/Rotate-Array/Solution.java
@@ -1,35 +1,30 @@
 1class Solution {
 2    public void rotate(int[] nums, int k) {
-3        if (nums.length == 1)
+3        if (nums == null || nums.length <= 1) {
 4            return;
-5            k = k % nums.length;
-6        if (nums.length < k) {
-7            while (k != 0) {
-8                int temp = nums[nums.length - 1];
-9                for (int i = nums.length - 1; i > 0; i--) {
-10                    nums[i] = nums[i - 1];
-11                }
-12                nums[0] = temp;
-13                k--;
-14            }
-15        }
-16        else{
-17            if(nums.length<2){
-18                return;
-19            }
-20            int arr[] = new int[nums.length];
-21            int j=0;
-22            for(int i=nums.length-k; i<nums.length;i++){
-23                arr[j]=nums[i];
-24                j++;
-25            }
-26            for(int i=0; i<nums.length-k;i++){
-27                arr[j]=nums[i];
-28                j++;
-29            }
-30            for(int i=0; i<nums.length; i++){
-31                nums[i]=arr[i];
-32            }
-33        }
-34    }
-35}
+5        }
+6        
+7        k = k % nums.length;
+8        if (k == 0) {
+9            return;
+10        }
+11        
+12        // 1. Reverse the entire array
+13        reverse(nums, 0, nums.length - 1);
+14        // 2. Reverse the first k elements
+15        reverse(nums, 0, k - 1);
+16        // 3. Reverse the remaining elements
+17        reverse(nums, k, nums.length - 1);
+18    }
+19    
+20    private void reverse(int[] nums, int start, int end) {
+21        while (start < end) {
+22            int temp = nums[start];
+23            nums[start] = nums[end];
+24            nums[end] = temp;
+25            start++;
+26            end--;
+27        }
+28    }
+29}
+30
