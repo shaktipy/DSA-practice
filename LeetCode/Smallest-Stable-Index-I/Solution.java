c0b15727eff403b8ce88a55c4d4f4ef58104b9b9
Shakti Vardhan Singh <shaktivardhans9@gmail.com>
2026-09-13 23:49:44 +0530

    Add solution for Smallest Stable Index I

--- LeetCode/Smallest-Stable-Index-I/Solution.java
+++ LeetCode/Smallest-Stable-Index-I/Solution.java
@@ -1,24 +1,16 @@
-1class Solution {
-2    public int firstStableIndex(int[] nums, int k) {
-3        for(int i=0;i<nums.length;i++){
-4            int max=nums[0];
-5            for(int j=1;j<=i;j++){
-6                if(max <= nums [j]){
-7                    max=nums[j];
-8                }
-9            }
-10            // System.out.println(max);
-11            int min=nums[i];
-12            for(int j=i+1;j<nums.length;j++){
-13                if(min >= nums[j]){
-14                    min=nums[j];
-15                }
-16            }
-17            // System.out.println(min);
-18            if(max-min <= k){
-19                return i;
-20            }
-21        }
-22        return -1;
-23    }
-24}
+[1,3,2,4,0]
+2
+[10,5,15,0,20]
+10
+[5,5,5,5,5,5]
+0
+[0,1,0,1,0,1]
+1
+[1,0,1,0,1,0]
+1
+[9,8,7,6,5,4,3,2,1,0]
+9
+[0,1,2,3,4,5,6,7,8,9]
+0
+[100,100,100,0,0,0]
+100
